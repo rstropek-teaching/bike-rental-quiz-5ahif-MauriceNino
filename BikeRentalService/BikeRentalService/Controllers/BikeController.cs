af2c2f41652c08b5e6e6fdc4aba5a2399b298b58
Maurice <mauriceprivat98@gmail.com>
2018-02-17 14:29:46 +0100

    Controllers for Bikes & Customers

---
+++ BikeRentalService/BikeRentalService/Controllers/BikeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BikeRentalService.Models;
+
+namespace BikeRentalService.Controllers
+{
+    [Route("api/[controller]")]
+    public class BikesController : Controller
+    {
+        private BikeRentalContext dbContext = new BikeRentalContext();
+
+
+        [HttpGet]
+        public IActionResult GetAllBikes()
+            => StatusCode(200, dbContext.Bikes.Select(e => e).ToList());
+
+        [HttpGet]
+        [Route("Available?{sort=null}")]
+        public IActionResult GetAllRentals(string sort)
+            => StatusCode(200, dbContext.Bikes.Select(e => e)
+                .Where(b =>
+                    //Select all Rentals of this bike where the RentEndTime is null
+                    dbContext.Rentals.Where(r => r.RentEndTime == null && r.RentedBike.BikeId == b.BikeId)
+                    //Check if the List of the active orders whith this bike is length 0
+                    .Select(r => r).ToArray().Length == 0)
+                .OrderBy(b => 
+                    sort=="pofh"?
+                        b.RentalPriceFirstHour:
+                        sort=="poah"?
+                            b.RentalPriceExtraHour:
+                            sort=="pd"?
+                                b.BikeId:b.BikeId
+                ));
+
+
+        [HttpPost]
+        [Route("Update/{bikeId}")]
+        public IActionResult Update(int bikeId, [FromBody]Bike bike)
+        {
+            Bike updateElement = dbContext.Bikes.Select(e => e).Where(e => e.BikeId == bikeId).FirstOrDefault();
+
+            updateElement.Brand = bike.Brand == null ? updateElement.Brand : bike.Brand;
+            updateElement.Category = bike.Category == null ? updateElement.Category : dbContext.BikeCategorys.Where(c => c.CategoryId == bike.Category.CategoryId).FirstOrDefault();
+            updateElement.DateOfLastService = bike.DateOfLastService == null ? updateElement.DateOfLastService : bike.DateOfLastService;
+            updateElement.Notes = bike.Notes == null ? updateElement.Notes : bike.Notes;
+            updateElement.PurchaseDate = bike.PurchaseDate == null ? updateElement.PurchaseDate : bike.PurchaseDate;
+            updateElement.RentalPriceExtraHour = bike.RentalPriceExtraHour == 0 ? updateElement.RentalPriceExtraHour : bike.RentalPriceExtraHour;
+            updateElement.RentalPriceFirstHour = bike.RentalPriceFirstHour == 0 ? updateElement.RentalPriceFirstHour : bike.RentalPriceFirstHour;
+
+            dbContext.SaveChanges();
+            return StatusCode(200, updateElement);
+        }
+
+        [HttpPut]
+        [Route("Create")]
+        public IActionResult Create([FromBody]Bike bike)
+        {
+            bike.Category = dbContext.BikeCategorys.Where(c => c.CategoryId == bike.Category.CategoryId).FirstOrDefault();
+            dbContext.Bikes.Add(bike);
+            dbContext.SaveChanges();
+            return StatusCode(200, bike);
+        }
+        
+        [HttpDelete]
+        [Route("Delete/{bikeId}")]
+        public IActionResult Delete(int bikeId)
+        {
+            Bike deletingBike = dbContext.Bikes.Where(b => b.BikeId == bikeId).FirstOrDefault();
+
+            if (deletingBike == null) return StatusCode(400, "Delete Failed");
+            if(dbContext.Rentals.Where(r=>r.RentedBike.BikeId==bikeId).ToArray().Length>0) return StatusCode(400, "Delete Failed");
+
+            dbContext.Bikes.Remove(deletingBike);
+            dbContext.SaveChanges();
+            return StatusCode(200, "Delete Successful");
+
+        }
+    }
+}
--- BikeRentalService/BikeRentalService/Controllers/CustomersController.cs
+++ BikeRentalService/BikeRentalService/Controllers/CustomersController.cs
@@ -3,42 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BikeRentalService.Models;
 
 namespace BikeRentalService.Controllers
 {
     [Route("api/[controller]")]
     public class CustomersController : Controller
     {
-        // GET api/values
+        private BikeRentalContext dbContext = new BikeRentalContext();
+
+
         [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        public IActionResult GetAllCustomers(string lastName) 
+            => StatusCode(200, dbContext.Customers.Select(e => e)
+                .Where(e => (lastName == null ? e.LastName : lastName) == e.LastName).ToList());
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        [HttpGet]
+        [Route("GetAllRentals/{custId}")]
+        public IActionResult GetAllRentals(int custId)
+            => StatusCode(200, dbContext.Rentals.Select(e => e)
+                .Where(e => e.Buyer.CustomerId == custId).ToList());
 
-        // POST api/values
+
         [HttpPost]
-        public void Post([FromBody]string value)
+        [Route("Update/{custId}")]
+        public IActionResult Update(int custId, [FromBody]Customer cust)
         {
+            Customer updateElement = dbContext.Customers.Select(e => e).Where(e => e.CustomerId == custId).FirstOrDefault();
+            updateElement.Birthday = cust.Birthday==null?updateElement.Birthday:cust.Birthday;
+            updateElement.FirstName = cust.FirstName == null ? updateElement.FirstName : cust.FirstName;
+            updateElement.LastName = cust.LastName == null ? updateElement.LastName : cust.LastName;
+            updateElement.Gender = cust.Gender==null?updateElement.Gender:dbContext.PersonGenders.Where(e=>e.GenderId==cust.Gender.GenderId).FirstOrDefault();
+            updateElement.HouseNumber = cust.HouseNumber == 0 ? updateElement.HouseNumber : cust.HouseNumber;
+            updateElement.Street = cust.Street == null ? updateElement.Street : cust.Street;
+            updateElement.Town = cust.Town == null ? updateElement.Town : cust.Town;
+            updateElement.ZipCode = cust.ZipCode == 0 ? updateElement.ZipCode : cust.ZipCode;
+
+            dbContext.SaveChanges();
+            return StatusCode(200, updateElement);
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        [HttpPut]
+        [Route("Create")]
+        public IActionResult Create([FromBody]Customer cust)
         {
+            cust.Gender = dbContext.PersonGenders.Where(e => e.GenderId == cust.Gender.GenderId).FirstOrDefault();
+            dbContext.Customers.Add(cust);
+            dbContext.SaveChanges();
+            return StatusCode(200, cust);
         }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        
+        [HttpDelete]
+        [Route("Delete/{custId}")]
+        public IActionResult Delete(int custId)
         {
+            Customer deletingCustomer = dbContext.Customers.Where(e => e.CustomerId == custId).FirstOrDefault();
+            if (deletingCustomer != null)
+            {
+                dbContext.Customers.Remove(deletingCustomer);
+                // Cascade Delte
+                dbContext.Rentals.RemoveRange(dbContext.Rentals.Where(e => e.Buyer.CustomerId == custId).ToArray());
+                dbContext.SaveChanges();
+                return StatusCode(200, "Delete Successful");
+            }
+            return StatusCode(400, "Delete Failed");
         }
     }
 }
